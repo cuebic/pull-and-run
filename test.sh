35d21110d8807737c3f483a8902886ba6691b90f
greg-chuchro <grzegorz-chuchro@cuebic.co.jp>
2020-01-31 16:09:25 +0900

    fix formatting

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 REPO_URI=$1
-echo $REPO_URI
 REPO_NAME=$(echo $REPO_URI | rev | cut -d "/" -f 1 | rev | cut -d "." -f 1)
-if [!-d ~/.ssh]; then
+if [ ! -d ~/.ssh ]; then
    mkdir -p ~/.ssh
 fi;
 echo $SSH_KEY > ~/.ssh/id_rsa
