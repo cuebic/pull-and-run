5c8b3614f24f3545ceb04e8fa4ec5b4655ab3031
greg-chuchro <grzegorz-chuchro@cuebic.co.jp>
2020-01-31 16:50:46 +0900

    fix test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,16 @@
+yes | apt-get update
+yes | apt-get install openssh-client git
+
 REPO_URI=$1
 REPO_NAME=$(echo $REPO_URI | rev | cut -d "/" -f 1 | rev | cut -d "." -f 1)
 if [ ! -d ~/.ssh ]; then
    mkdir -p ~/.ssh
 fi;
-echo $SSH_KEY > ~/.ssh/id_rsa
+echo $SSH_KEY | awk '{gsub(/\\n/,"\n")}1' > ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
 eval `ssh-agent -s`
 ssh-add
 
 mkdir pull-and-run-tmp
 cd pull-and-run-tmp
-git clone $REPO_URI $REPO_NAME
+yes | git clone $REPO_URI $REPO_NAME
